3723fead08e7b69ede0ab26794a5a782657b8d84
AkarshG1 <akarsh98@gmail.com>
2019-08-29 21:48:37 +0200

    Added RL

--- Reinforcement_Learning/cartpole_pg.py
+++ Reinforcement_Learning/cartpole_pg.py
@@ -1,3 +1,11 @@
 import gym
 from atlas_ml import *
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(10000):
+    env.render()
+    __,__,done,__ = env.step(env.action_space.sample()) # take a random action
+    if done==True:
+        env.reset()
+env.close()
 
